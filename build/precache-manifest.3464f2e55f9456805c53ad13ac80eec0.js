47c906e5438e7af58d3c7d397191f6c9f19331bb
david <davidfarley7171@gmail.com>
2020-09-07 13:45:29 -0700

    removed the build folder from the gitignore

---
+++ build/precache-manifest.3464f2e55f9456805c53ad13ac80eec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ae3a00322f975e6e612d0df7cc757a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32045b061d3ff736fc07",
+    "url": "./static/css/main.c0b41f0f.chunk.css"
+  },
+  {
+    "revision": "2424d4f00abe2bca0498",
+    "url": "./static/js/2.534c13fd.chunk.js"
+  },
+  {
+    "revision": "32045b061d3ff736fc07",
+    "url": "./static/js/main.dd299c91.chunk.js"
+  },
+  {
+    "revision": "6daaf64b785fd063f7b9",
+    "url": "./static/js/runtime-main.be1ad600.js"
+  }
+]);
